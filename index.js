db79b1ce273c2390d8f9624f8c3e1d3f384e67d0
Blake Guilloud <fbguillo@gmail.com>
2017-01-26 00:04:32 -0500

    initial migration and model

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log('hello');
+import express    from 'express';
+import bodyParser from 'body-parser';
+import { Model }  from 'objection';
+import cors       from 'cors';
+import path       from 'path';
+
+import todoRouter  from './routers/Todo.router';
+
+import knex       from 'knex';
+import knexConfig from './knexfile.js';
+
+const connection = knex(knexConfig);
+
+knex(knexConfig).migrate.latest();
+
+Model.knex(connection);
+
+const app = express();
+
+app.set('appPath', path.join('', 'client'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(`${app.get('appPath')}/build`));
+
+// app.use('/api/todos', todoRouter);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log('app listening on port: ', port);
+});
---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'heyo',
+    },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
