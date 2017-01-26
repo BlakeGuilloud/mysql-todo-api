4b8501f7f567a3f9e4c53307542d48c734bc22e7
Blake Guilloud <fbguillo@gmail.com>
2017-01-26 00:16:33 -0500

    brought in fluyt for promise help

--- src/controllers/Todo.controller.js
+++ src/controllers/Todo.controller.js
@@ -1,29 +1,32 @@
 import * as todoService from '../services/Todo.service';
 import { SendSuccess, SendError } from 'tael';
+import Fluyt from 'fluyt';
 
 export function postTodo(req, res) {
-  Promise.resolve()
-    .then(() => todoService.postTodo(req.body))
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(todoService.postTodo, 'data'))
     .then(SendSuccess(res))
     .catch(SendError(res));
 }
 
 export function getTodos(req, res) {
-  return Promise.resolve()
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(todoService.getTodos))
     .then(() => todoService.getTodos())
     .then(SendSuccess(res))
     .catch(SendError(res));
 }
 
 export function patchTodo(req, res) {
-  Promise.resolve()
-    .then(() => todoService.patchTodo(req.params.id, req.body))
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(todoService.patchTodo, 'params.id', 'data'))
     .then(SendSuccess(res))
     .catch(SendError(res));
 }
 
 export function deleteTodo(req, res) {
-  Promise.resolve()
+  return Promise.resolve(Fluyt.liftRequest(req))
+    .then(Fluyt.liftFunction(todoService.deleteTodo, 'params.id'))
     .then(() => todoService.deleteTodo(req.params.id))
     .then(SendSuccess(res))
     .catch(SendError(res));
--- src/routes/Todo.router.js
+++ src/routes/Todo.router.js
@@ -1,4 +1,4 @@
-import express             from 'express';
+import express from 'express';
 import * as todoController from '../controllers/Todo.controller';
 
 const router = express.Router();
